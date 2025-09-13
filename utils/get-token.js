9903c1175ec40985221538973f5f9cc011901534
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-09-14 01:14:21 +0700

    Update get-token.js

--- utils/get-token.js
+++ utils/get-token.js
@@ -4,5 +4,3 @@ export const token = async () => {
   const res = await axios.get("https://dramabox-token.vercel.app/token");
   return res.data; // { token, deviceid }
 };
-
-export default { token };
