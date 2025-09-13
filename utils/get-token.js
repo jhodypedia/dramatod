f659faac1d110e33ef8503a103b4000afeac48e8
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-09-13 23:13:17 +0700

    Create get-token.js

---
+++ utils/get-token.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const token = async () => {
+  const res = await axios.get("https://dramabox-token.vercel.app/token");
+  return res.data; // { token, deviceid }
+};
+
+export default { token };
